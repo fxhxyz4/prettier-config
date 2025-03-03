a17bf4dc71f25389794081ccfe3e97bcaf007a83
fxhxyz <fxhxyz8@gmail.com>
2025-03-03 18:42:13 +0000

    _

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,10 @@
+/**
+* @F @fxhxyz/prettier-config v1.0.3
+* @F (c)2025 fxhxyz
+* @F MIT License
+* @F github.com/fxhxyz4/prettier-config
+*/
+
 import config from "@fxhxyz/prettier-config";
 
 /** @type {import("prettier").Config} */
