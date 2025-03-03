19001449a01c9e4687f1ec9a13f0aafef9915f66
fxhxyz <fxhxyz8@gmail.com>
2025-03-03 17:53:18 +0000

    update release script

--- release.sh
+++ release.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-echo "🔧 Building the project..."
-npm run build
-
-VERSION_TYPE=$1
-
-if [ -z "$VERSION_TYPE" ]; then
-  VERSION_TYPE="patch"
-fi
-
 echo "🚀 Bumping version ($VERSION_TYPE)..."
 npm version $VERSION_TYPE
 
