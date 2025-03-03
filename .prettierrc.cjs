dd48786e955ad8856836d8686a654b9da1a5be21
fxhxyz <fxhxyz8@gmail.com>
2025-03-03 18:41:55 +0000

    builded cjs version

---
+++ .prettierrc.cjs
@@ -0,0 +1,19 @@
+/**
+* @F @fxhxyz/prettier-config v1.0.3
+* @F (c)2025 fxhxyz
+* @F MIT License
+* @F github.com/fxhxyz4/prettier-config
+*/
+
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var config = require('@fxhxyz/prettier-config');
+
+/** @type {import("prettier").Config} */
+var _prettierrc = {
+  ...config
+};
+
+exports.default = _prettierrc;
