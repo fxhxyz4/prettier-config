bbcba809abd2a9e2c124fa5d129e7b744344e05c
fxhxyz <fxhxyz8@gmail.com>
2025-03-03 18:42:18 +0000

    _

--- release.sh
+++ release.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+echo "🔧 Building the project..."
+npm run build
+
 VERSION_TYPE=$1
 
 if [ -z "$VERSION_TYPE" ]; then
-  VERSION_TYPE="patch"
+    VERSION_TYPE="patch"
 fi
 
 echo "🚀 Bumping version ($VERSION_TYPE)..."
